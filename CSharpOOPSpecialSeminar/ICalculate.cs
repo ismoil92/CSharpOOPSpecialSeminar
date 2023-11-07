95095d2119fff1b5f076f4a0dbc09cad2d47c9cf
Ismoil Muhammadiev <ism0il.muhammadiev92@gmail.com>
2023-11-07 19:07:26 +0500

    4 домашняя работа выполнена.

---
+++ CSharpOOPSpecialSeminar/ICalculate.cs
@@ -0,0 +1,14 @@
+﻿namespace CSharpOOPSpecialSeminar;
+
+public interface ICalculate
+{
+    double Result { get; set; }
+    void Sum(int x);
+    void Subtract(int x);
+    void Multiply(int x);
+    void Divide(int x);
+
+    void CancelLast();
+
+    event EventHandler<EventArgs> MyEventHandler;
+}
