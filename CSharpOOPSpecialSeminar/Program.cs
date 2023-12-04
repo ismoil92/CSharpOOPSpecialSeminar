d721922af02051b41c51cac820afe83dfedbff41
Ismoil Muhammadiev <ism0il.muhammadiev92@gmail.com>
2023-12-04 12:22:59 +0500

    Домашняя работа номер 8 выполнена.

--- CSharpOOPSpecialSeminar/Program.cs
+++ CSharpOOPSpecialSeminar/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CSharpOOPSpecialSeminar;
+
+Lesson8.FindFileNameRecurs("C:\\Users\\User\\Desktop", "222.txt", "559");
